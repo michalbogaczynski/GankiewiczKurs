e413d2baabe65604a34a35814684e93ba9b54c5a
michalbogaczynski <bogaczynski.m@gmail.com>
2018-10-16 11:09:51 +0200

    Episode 2 Dziedziczenie, polimorfizm, klasa abstrakcyjna

---
+++ DziedziczenieE2/Car.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DziedziczenieE2
+{
+    public abstract class Car
+    {
+        public double Acceleration { get; protected set; } = 10;
+        public double Speed { get; protected set; } = 100;
+
+
+        public void Start()
+        {
+            Console.WriteLine("Turning on the engine");
+            Console.WriteLine($"Running at:{Speed} km/h");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Stopping the car");
+        }
+
+        public virtual void Accelerate()
+        {
+            Console.WriteLine("Accelerating...");
+            Speed += Acceleration;
+            Console.WriteLine($"Running at:{Speed} km/h");
+        }
+
+        public abstract void Boost();
+    
+    }
+
+    public class Truck : Car
+    {
+        public override void Accelerate()
+        {
+            Console.WriteLine("Accelerating a truck");
+            base.Accelerate(); // odwołuje się do klasy bazowej, wykonywana jest metoda z klasy bazowej
+        }
+
+        public override void Boost()
+        {
+            Console.WriteLine("Boositng a truck");
+            Speed += 50;
+            Console.WriteLine($"Running at:{Speed} km/h");
+        }
+    }
+
+    public class SportCar : Car
+    {
+        public override void Accelerate()
+        {
+            Console.WriteLine("Accelerating a sport car");
+            base.Accelerate();
+        }
+
+        public override void Boost()
+        {
+            Console.WriteLine("Boosting a sport car...");
+            Speed += 100;
+            Console.WriteLine($"Running at:{Speed} km/h");
+        }
+    }
+
+    public class Race
+    {
+        public void Begin()
+        {
+            SportCar sportcar = new SportCar();
+            Truck truck = new Truck();
+
+            List<Car> cars = new List<Car>
+            {
+                sportcar, truck
+            };
+
+            foreach (Car car in cars)
+            {
+                car.Start();
+                car.Accelerate();
+                car.Boost();
+            }
+        }
+    }
+}
---
+++ DziedziczenieE2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DziedziczenieE2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Race race = new Race();
+            race.Begin();
+            Console.ReadKey();
+        }
+    }
+}
